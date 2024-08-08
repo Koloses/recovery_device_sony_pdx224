84b49a7e69899012d0338958a5347261260f2e77
Krzysztof Kolendowicz <koloses@gmail.com>
2024-08-08 17:14:56 +0200

    Enable touchscreen

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -94,4 +94,5 @@ SLOT=$(getprop ro.boot.slot_suffix)
 mount /dev/block/bootdevice/by-name/modem$SLOT /firmware -O ro
 echo "1" > /proc/sys/kernel/firmware_config/force_sysfs_fallback
 echo "1" > /sys/kernel/boot_adsp/boot
+echo "1" > /sys/devices/common_touch/touch/after_work
 exit 0
